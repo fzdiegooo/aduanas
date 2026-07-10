b2d903bb12083c8dbc551ddbd268f87354ddd925
fzdiegooo <zxiteft21@gmail.com>
2026-07-10 18:41:18 -0500

    refactor: update senasa route path to be nested under manifiestos endpoint

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors({
 app.use(express.json());
 
 app.use('/api/manifiestos', manifiestos);
-app.use('/api/senasa', senasa);
+app.use('/api/manifiestos/senasa', senasa);
 
 app.get('/health', (_req, res) => res.json({ ok: true }));
 
